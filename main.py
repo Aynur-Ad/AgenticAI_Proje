5cb22a7cab57a72412b385ce322d607bceefc8a5
Aynur Adıbelli <aynuradibelli27@gmail.com>
2026-01-10 20:47:33 +0300

    Uncomment sys.path.append for module import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Proje dizinini yola ekle (Import hatası almamak için şart)
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     print("==========================================")
